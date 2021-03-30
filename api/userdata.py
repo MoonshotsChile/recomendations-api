29e788baad37519b38ac2f2a95f7b8a4e0b6e76b
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-30 15:11:29 -0300

    API RC

--- api/userdata.py
+++ api/userdata.py
@@ -1,4 +1,5 @@
 import json
+from urllib import request
 from urllib.request import urlopen
 from urllib.parse import urlparse
 from flask import jsonify
@@ -11,5 +12,32 @@ class Userdata:
         query = urlparse(_request.url).query
         url = f'{API_URL}/userdata/{_id}?{query}'
         json_url = urlopen(url)
+        data = json.loads(json_url.read())
+        return jsonify(data)
+
+    @staticmethod
+    def patch(_request, _id):
+        url = f'{API_URL}/userdata/{_id}'
+        req = request.Request(url, method='PATCH', data=_request.data)
+        req.add_header('Content-Type', 'application/json')
+        json_url = urlopen(req)
+        data = json.loads(json_url.read())
+        return data
+
+    @staticmethod
+    def add(_request, _id):
+        url = f'{API_URL}/userdata/{_id}'
+        req = request.Request(url, method='POST', data=_request.data)
+        req.add_header('Content-Type', 'application/json')
+        json_url = urlopen(req)
+        data = json.loads(json_url.read())
+        return jsonify(data)
+
+    @staticmethod
+    def delete(_request, _id):
+        url = f'{API_URL}/userdata/{_id}'
+        req = request.Request(url, method='DELETE')
+        req.add_header('Content-Type', 'application/json')
+        json_url = urlopen(req)
         data = json.loads(json_url.read())
         return jsonify(data)
--- recommendations_api.py
+++ recommendations_api.py
@@ -1,11 +1,8 @@
 import os
-from urllib.request import urlopen
-from urllib.parse import urlparse
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from waitress import serve
-import json
 
 from api.benefits import Benefits
 from api.missions import Missions
@@ -15,31 +12,46 @@ from api.userdata import Userdata
 app = Flask(__name__)
 CORS(app)
 
-API_URL = "https://api.moonshots.cl/benefits"
-
 
 @app.route('/benefits-v2/benefits/', defaults={'_id': ''}, methods=["GET"])
-@app.route('/benefits-v2/benefits/<_id>', methods=["GET"])
+@app.route('/benefits-v2/benefits/<_id>/', methods=["GET"])
 def benefits(_id):
     return Benefits.get(request, _id)
 
 
-@app.route('/benefits-v2/userdata/', defaults={'_id': ''}, methods=["GET"])
-@app.route('/benefits-v2/userdata/<_id>', methods=["GET"])
-def userdata(_id):
-    return Userdata.get(request, _id)
-
-
 @app.route('/benefits-v2/missions/', defaults={'_id': ''}, methods=["GET"])
-@app.route('/benefits-v2/missions/<_id>', methods=["GET"])
+@app.route('/benefits-v2/missions/<_id>/', methods=["GET"])
 def missions(_id):
     return Missions.get(request, _id)
 
 
 @app.route('/benefits-v2/notifications/', defaults={'_id': ''}, methods=["GET"])
-@app.route('/benefits-v2/notifications/<_id>', methods=["GET"])
+@app.route('/benefits-v2/notifications/<_id>/', methods=["GET"])
 def notifications(_id):
     return Notifications.get(request, _id)
+
+
+@app.route('/benefits-v2/userdata/', defaults={'_id': ''}, methods=["GET"])
+@app.route('/benefits-v2/userdata/<_id>/', methods=["GET"])
+def userdata(_id):
+    return Userdata.get(request, _id)
+
+
+@app.route('/benefits-v2/userdata/', defaults={'_id': ''}, methods=["PATCH"])
+@app.route('/benefits-v2/userdata/<_id>/', methods=["PATCH"])
+def userdata_patch(_id):
+    return Userdata.patch(request, _id)
+
+
+@app.route('/benefits-v2/userdata/<_id>/', methods=["POST"])
+def userdata_add(_id):
+    return Userdata.add(request, _id)
+
+
+@app.route('/benefits-v2/userdata/', defaults={'_id': ''}, methods=["DELETE"])
+@app.route('/benefits-v2/userdata/<_id>/', methods=["DELETE"])
+def userdata_delete(_id):
+    return Userdata.delete(request, _id)
 
 
 def is_port_in_use(_port):
